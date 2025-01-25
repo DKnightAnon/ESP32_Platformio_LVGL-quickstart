721961d05f6a1989a7ce5fc6c2bba470f4bb270f
DKnightAnon <a.r.c.delgado@outlook.com>
2025-01-25 13:11:08 +0100

    Improved set_var_arc_value() to limit lower and upper bounds of value

--- src/UI/vars.c
+++ src/UI/vars.c
@@ -7,12 +7,12 @@ int32_t get_var_arc_value() {
 }
 
 void set_var_arc_value(int32_t value) {
-    // if ((arc_value - value )< 0){
-
-    // } else if ((arc_value + value) > 100) {
-
-    // } else{
+    if (value < 0){
+        arc_value = 0;
+    } else if (value > 100) {
+        arc_value = 100;
+    } else{
         arc_value = value;
-    // }
+    }
     
 }
